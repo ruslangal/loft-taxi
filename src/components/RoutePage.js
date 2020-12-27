4af075f31afc6878a90500c3222cd5f3499fcfe7
Ruslan Galeev (elregent) <elregent@yandex-team.ru>
2020-12-27 14:16:58 +0300

    feat(loft-taxi): добавил карту mapbox-gl

--- src/components/RoutePage.js
+++ src/components/RoutePage.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import './RoutePage.css';
-//import RouteForm from './RouteForm'
+import mapboxgl from 'mapbox-gl'
+
+const mapboxToken = "pk.eyJ1IjoicmVnZW50bSIsImEiOiJja2o2emIzZGY2Y3lqMnFsYnBlYmx3MGJmIn0.NGD8dttjQ6G1OBU4rEVpVA";
 
 class RoutePage extends Component {
+    map = null;
+    mapContainer = React.createRef();
+
+    componentDidMount() {
+        mapboxgl.accessToken = mapboxToken;
+
+        this.map = new mapboxgl.Map({
+            container: this.mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v11",
+            zoom: 11,
+            center: [10.39170933059438, 63.4309997568285],
+            controls: false,
+        });
+    }
+
+    componentWillUnmount() {
+        this.map.remove();
+    }
+
     render() {
         return (
             <div className="route-page">
-                {/*<RouteForm {...this.props}/>*/}
+                <div
+                    data-testid="map"
+                    className="route-page__map"
+                    ref={this.mapContainer}
+                />
             </div>
         )
     }
